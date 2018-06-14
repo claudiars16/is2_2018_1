835f10856c73e0186fc0a5d8f014a4089d765e7f
Joaquín Casanova <jcasanovam11@gmail.com>
2018-06-14 12:51:04 -0500

    cambiar tamaño de pantalla

--- bin/config.py
+++ bin/config.py
@@ -1,6 +1,24 @@
 TITLE = "Inka challenge"
-WIDTH  = 1200
-HEIGHT = 800
+WIDTH  = 960
+HEIGHT = 640
 HW, HH = WIDTH / 2, HEIGHT / 2
 FPS = 500
-BLACK = (255,0,0)
+BLACK = (0,0,0)
+YELLOW  = (255, 255, 0)
+GREEN = (0,128,0)
+RED = (255,0,0)
+BLUE = (0,0,255)
+PURPLE = (144, 53, 192)
+GRAY = (96,96,96)
+ORANGE = (255 ,87 , 51)
+
+PLAYER_ACC_1 = 0.4
+PLAYER_ACC_2 = 0.8
+PLAYER_ACC_3 = 1.2
+
+PLAYER_FRIC = -0.1
+PLAYER_GRAV = 0.4
+
+FLOOR = 583
+
+URL_API = "https://inkachallenge.herokuapp.com/api"
