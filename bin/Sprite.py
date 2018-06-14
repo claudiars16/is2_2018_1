ced7dfc961343c1ad113ec7a6a1c3f150c6e9589
claudia martinez <claudia.cgmg.06@gmail.com>
2018-06-14 13:08:05 -0500

    add food

--- bin/Sprite.py
+++ bin/Sprite.py
@@ -117,3 +117,16 @@ class Tumi(pg.sprite.Sprite):
 
     def update(self , dis):
         self.rect.x -= dis
+
+class Food(pg.sprite.Sprite):
+
+    def __init__(self , x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load("../assets/one/food.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+
+    def update(self , dis):
+        self.rect.x -= dis
+    
