97bcd4657762473d85994ed2b0e9b435637be128
claudia martinez <claudia.cgmg.06@gmail.com>
2018-07-12 15:08:04 -0500

    add level dos

--- bin/Stage.py
+++ bin/Stage.py
@@ -740,3 +740,346 @@ class SelectLevel():
 
         self.arrayComponente.append(self.back)
 
+class NivelTwo():
+    def __init__(self, game, win):
+        self.game = game
+        self.win = win
+        self.arrayComponente = []
+        self.bg = Component(win, pg.image.load(
+            "../assets/two/f.png"), None, 0, 0, 0)
+        self.floor = Component(win, pg.image.load(
+            "../assets/two/floor.png"), None, 0, FLOOR, 0)    
+        self.win.blit(self.bg.currentImage, (0, 0))
+        self.bgWidth, self.bgHeight = self.bg.currentImage.get_rect().size
+        self.stageWidth = self.bgWidth * 4
+        self.startScrollingPosX = HW
+        self.stagePosX = 0
+        self.pauseState = False
+        self.loseState = False
+        self.winState = False
+        self.name = ""
+        # CONJUNTO DE IMAGENES
+        self.players = pg.sprite.Group()
+        self.enemys = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.flags = pg.sprite.Group()
+        self.bases = pg.sprite.Group()
+        self.lifes = pg.sprite.Group()
+        self.tumis = pg.sprite.Group()
+        self.foods = pg.sprite.Group()
+        self.bullets = pg.sprite.Group()
+        self.bullets_enemy = pg.sprite.Group()
+        self.spritesheet = Spritesheet("../assets/two/player{}.png".format(self.game.suit))
+        self.spritesheet_enemy = Spritesheet("../assets/two/enemy2.png")
+        #ADD PLAYER
+        self.player = Player(self)
+        self.players.add(self.player)
+        #ADD ENEMY
+        self.enemy1 = Enemy(self, 600, FLOOR-65,2)
+        self.enemy2 = Enemy(self, 1000, FLOOR-65,2)
+        self.enemys.add(self.enemy1)
+        self.enemys.add(self.enemy2)
+        #ADD PLATFORMS
+        base = Platform(0, FLOOR , 3*WIDTH, HEIGHT - FLOOR,"")
+        base.image.set_alpha(0) 
+        p1 = Platform(750 , FLOOR - 100 , 100 , 20,"1")
+        p2 = Platform(1135.5 , FLOOR - 100 , 100 , 20,"1")
+        p3 = Platform(1411 , FLOOR - 200 , 50 , 20,"2")
+        p4 = Platform(1636.5 , FLOOR - 100 , 100 , 20,"1")
+        p5 = Platform(2350 , FLOOR - 100 , 100 , 20,"1")
+        p6 = Platform(3055.5 , FLOOR - 100 , 100 , 20,"1")
+        p7 = Platform(3331 , FLOOR - 200 , 50 , 20,"2")
+        p8 = Platform(3556.5 , FLOOR - 100 , 100 , 20,"1")
+        self.bases.add(base)
+        self.platforms.add(p1)
+        self.platforms.add(p2)
+        self.platforms.add(p3)
+        self.platforms.add(p4)
+        self.platforms.add(p5)
+        self.platforms.add(p6)
+        self.platforms.add(p7)
+        self.platforms.add(p8)
+        #ADD LIFE
+        l1 = Life(1423.5 , FLOOR - 225)
+        l2 = Life(3343.5 , FLOOR - 225)
+        self.lifes.add(l1)
+        self.lifes.add(l2)
+        #ADD TUMIS
+        t1 = Tumi(1674 ,FLOOR - 150)
+        t2 = Tumi(3093 ,FLOOR - 150)
+        self.tumis.add(t1)
+        self.tumis.add(t2)
+        #ADD FOOD
+        f1 = Food(1160 , FLOOR - 125)
+        self.foods.add(f1)
+        #ADD FLAG
+        flag  = Flag(self.stageWidth-30,FLOOR-100)
+        self.flags.add(flag)
+        #LETRA
+        self.myfont = pg.font.SysFont("monospace", 20, True)
+        self.myfontNumber = pg.font.SysFont("monospace", 30, True)
+        #PUNTAJE
+        self.poits = 0
+        #LIFES
+        self.lifes_points = 1
+        #FOOD
+        self.nfoods = 0
+        #LOAD IMAGES
+        self.image_life = pg.image.load('../assets/two/life.png')
+        #LOAD COMPONENTS
+        self.arrayComponents = []
+        self.arrayPause = []
+        self.arrayLose = []
+        self.arrayWin = []
+        self.pause = Component(win , pg.image.load(
+            "../assets/two/pause.png") , None , 905 , 5 , 0)
+        #LOAD COMPONENTS PAUSE
+        self.marco_pause = Component(win , pg.image.load(
+            "../assets/pause/pause_marco.png") , None ,280 , 160 , 0)
+        self.play = Component(win, pg.image.load("../assets/pause/btn_play.png"),
+                              pg.image.load("../assets/pause/btn_alt_play.png"), 325.3, 250, 1)
+        self.exit = Component(win, pg.image.load("../assets/pause/btn_exit.png"),
+                              pg.image.load("../assets/pause/btn_alt_exit.png"), 502.6, 250, 1)
+        
+        #LOAD COMPONENTS LOSE / WIN
+        self.marco = Component(win , pg.image.load(
+            "../assets/lose/tabla.png") , None ,230 , 70 , 0)
+        self.marco_win = Component(win , pg.image.load(
+            "../assets/lose/tabla_win.png") , None ,230 , 70 , 0)
+        self.game_over_title = Component(win , pg.image.load(
+            "../assets/lose/game_over_title.png") , None ,262 , 127 , 0)
+        self.win_title = Component(win , pg.image.load(
+            "../assets/lose/win_title.png") , None ,262 , 127 , 0)
+        self.save = Component(win, pg.image.load("../assets/lose/btn_guardar.png"),
+                              pg.image.load("../assets/lose/btn_alt_guardar.png"), 325.3, 453, 1)
+        self.exit_lose = Component(win, pg.image.load("../assets/lose/btn_exit.png"),
+                              pg.image.load("../assets/lose/btn_alt_exit.png"), 502.6, 453, 1)
+        self.continue_win = Component(win, pg.image.load("../assets/lose/btn_exit.png"),
+                              pg.image.load("../assets/lose/btn_alt_exit.png"), 414, 368, 1)
+        self.player_dead = Component(win, pg.image.load(
+            "../assets/lose/player_{}_dead.png".format(self.game.suit)), None, 415, 250, 1)
+        self.player_win = Component(win, pg.image.load(
+            "../assets/lose/player_{}_win.png".format(self.game.suit)), None, 415, 250, 1)
+        self.lose_tumi = Component(win, pg.image.load(
+            "../assets/lose/tumi.png"), None, 310, 250, 1)
+        self.lose_food = Component(win, pg.image.load(
+            "../assets/lose/food.png"), None, 600, 270, 1)
+        
+        self.__loadComponents()
+        
+    def draw(self):
+        self.bases.draw(self.win)
+        self.players.draw(self.win)
+        self.enemys.draw(self.win)
+        self.platforms.draw(self.win)
+        self.lifes.draw(self.win)
+        self.tumis.draw(self.win)
+        self.foods.draw(self.win)
+        self.bullets.draw(self.win)
+        self.bullets_enemy.draw(self.win)
+        self.flags.draw(self.win)
+        #RENDER POINTS
+        label = self.myfont.render("PUNTAJE : {}".format(self.poits), 1, BLACK)
+        self.win.blit(label, (5, 0))
+        #RENDER LIFE
+        lifes = self.myfont.render("VIDAS : ", 1, BLACK)
+        place  = HW +  100
+        self.win.blit(lifes, (HW, 0))
+        for l in range(self.lifes_points):
+            self.win.blit(self.image_life , (place, 0))
+            place += 30
+        for component in self.arrayComponents:
+            component.draw()
+        if  self.pauseState:
+            for component in self.arrayPause:
+                component.draw()
+                component.hover()
+        if self.loseState:
+            for component in self.arrayLose:
+                component.draw()
+                component.hover()
+            #RENDER NAME
+            pg.draw.rect(self.win,(255,255,255),(330,400,300,40))
+            n = self.myfont.render(self.name, 1, BLACK)
+            self.win.blit(n, (350,410))
+            s_tumis = self.myfontNumber.render(str(self.poits), 1, BLACK)
+            s_food = self.myfontNumber.render(str(self.nfoods), 1, BLACK)
+            self.win.blit(s_tumis, (320, 355))
+            self.win.blit(s_food, (620, 330))
+        if self.winState:
+            for component in self.arrayWin:
+                component.draw()
+                component.hover()
+            s_tumis = self.myfontNumber.render(str(self.poits), 1, BLACK)
+            s_food = self.myfontNumber.render(str(self.nfoods), 1, BLACK)
+            self.win.blit(s_tumis, (320, 355))
+            self.win.blit(s_food, (620, 330))
+
+    def events(self):
+        mouse = pg.mouse.get_pos()
+        move = 0
+        k = pg.key.get_pressed()
+        if k[pg.K_RIGHT]:
+            move  = 1
+            self.player.side = 1
+        elif k[pg.K_LEFT]:
+            move = -1
+            self.player.side = -1
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if not self.pauseState and  not self.loseState and not self.winState:
+                if event.type == pg.KEYDOWN:
+                    if  event.key == pg.K_UP:
+                        self.player.jump(2)
+                    if event.key == pg.K_SPACE:
+                        self.player.shoot()
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if self.pause.inside(mouse[0], mouse[1]):
+                        self.goPause(True)
+            if self.pauseState:
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if self.play.inside(mouse[0], mouse[1]):
+                        self.goPause(False)
+                    if self.exit.inside(mouse[0], mouse[1]):
+                        self.goMenu()
+            if self.loseState:
+                if event.type == pg.KEYDOWN:
+                    if event.unicode.isalpha():
+                        self.name += event.unicode
+                    elif event.key == pg.K_BACKSPACE:
+                        self.name = self.name[:-1]
+                    elif event.key == pg.K_RETURN:
+                        self.name = ""
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if self.save.inside(mouse[0], mouse[1]):
+                        self.save_score()
+                    if self.exit_lose.inside(mouse[0], mouse[1]):
+                        self.goMenu()
+            if self.winState:
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if self.continue_win.inside(mouse[0], mouse[1]):
+                        self.goMenu()
+                        
+
+        if not self.pauseState and not self.loseState and not self.winState:
+            self.player.move(move)
+            self.move_screen(move)
+
+    def update(self):
+        if not self.pauseState and  not self.loseState and not self.winState:
+            self.bullets.update()
+            self.bullets_enemy.update()
+        for enemy in self.enemys:
+            enemy.animate()
+        self.player.jumping = True
+        #CHECK IF PLAYER HIT THE FLOOR
+        hit_floor = pg.sprite.spritecollide(self.player , self.bases , False)
+        if hit_floor:
+                self.player.jumping = False
+                self.player.pos.y = hit_floor[0].rect.top - 2
+                self.player.vel.y = 0
+
+        #CHECK IF PLAYER HIT A PLATFOR
+        if  self.player.vel.y > 0:
+            hits_platfroms = pg.sprite.spritecollide(self.player , self.platforms , False)
+            if hits_platfroms:
+                self.player.jumping = False
+                self.player.pos.y = hits_platfroms[0].rect.top - 2
+                self.player.vel.y = 0
+
+        #CHECK HIT A LIFE
+        hits_lifes = pg.sprite.spritecollide(self.player, self.lifes , False)
+        if hits_lifes:
+            self.game.life_sound()
+            self.lifes.remove(hits_lifes[0])
+            self.lifes_points += 1
+        #CHECK HIT A TUMI
+        hits_tumis = pg.sprite.spritecollide(self.player, self.tumis , False)
+        if hits_tumis:
+            self.game.coin_sound()
+            self.tumis.remove(hits_tumis[0])
+            self.poits += 10
+        #CHECK HIT A FOOD
+        hits_food = pg.sprite.spritecollide(self.player, self.foods , False)
+        if hits_food:
+            self.game.food_sound()
+            self.foods.remove(hits_food[0])
+            self.nfoods += 5
+        
+        #CHECK HIT TO ENMY
+        hits_enemy = pg.sprite.spritecollide(self.player, self.enemys , False)
+        if hits_enemy:
+            self.loseState = True
+        
+        #CHECK HIT TO ENMY BULLET
+        hits_enemy = pg.sprite.spritecollide(self.player, self.bullets_enemy , False)
+        if hits_enemy:
+            self.loseState = True
+        
+        #CHECK IF BULLET TO ENEMY
+        for bullet in self.bullets:
+            hits_bullet =  pg.sprite.spritecollide(bullet, self.enemys , True)
+            hits_b_b = pg.sprite.spritecollide(bullet, self.bullets_enemy , True)
+            if hits_bullet:
+                bullet.kill()
+                self.poits += 30
+            if hits_b_b:
+                bullet.kill()
+        
+        #CHECK IF WIIN
+        hits_flag = pg.sprite.spritecollide(self.player, self.flags , False)
+        if hits_flag:
+            self.winState = True
+
+    def __loadComponents(self):
+        self.arrayComponents.append(self.pause)
+        self.arrayPause.append(self.marco_pause)
+        self.arrayPause.append(self.play)
+        self.arrayPause.append(self.exit)
+        self.arrayLose.append(self.marco)
+        self.arrayLose.append(self.exit_lose)
+        self.arrayLose.append(self.save)
+        self.arrayLose.append(self.game_over_title)
+        self.arrayLose.append(self.player_dead)
+        self.arrayLose.append(self.lose_tumi)
+        self.arrayLose.append(self.lose_food)
+        self.arrayWin.append(self.marco_win)
+        self.arrayWin.append(self.continue_win)
+        self.arrayWin.append(self.win_title)
+        self.arrayWin.append(self.player_win)
+        self.arrayWin.append(self.lose_tumi)
+        self.arrayWin.append(self.lose_food)
+
+    def goPause(self, state):
+        self.pauseState = state
+
+    def goMenu(self):
+        self.game.changeState(MenuStage(self.game, self.win))
+
+    def move_screen(self, dir):
+        rel_x = round(self.stagePosX % self.bgWidth,0)
+        self.win.blit(self.bg.currentImage,
+                            (rel_x - self.bgWidth, 0))
+        self.win.blit(self.floor.currentImage,
+                            (rel_x - self.bgWidth, FLOOR))
+        
+        if rel_x < WIDTH:
+            self.win.blit(self.bg.currentImage, (rel_x, 0))
+            self.win.blit(self.floor.currentImage, (rel_x, FLOOR))
+        if self.player.rect.center[0] == self.startScrollingPosX:
+            self.platforms.update(self.player.des)
+            self.lifes.update(self.player.des)
+            self.tumis.update(self.player.des)
+            self.foods.update(self.player.des)
+            self.enemys.update(self.player.des)
+            self.flags.update(self.player.des)
+    
+    def save_score(self):
+        payload = {"nombre" : self.name , "puntaje" : self.poits}
+        r = requests.post(URL_API + "/ranking", data=payload)
+        print("Status : {}".format(r.status_code))
+        self.goMenu()
+
+
