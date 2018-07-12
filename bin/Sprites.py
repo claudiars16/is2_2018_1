37f02489d855638489ff31f83fd1744fd0d855c4
Joaquin Casanova <jcasanovam11@gmail.com>
2018-07-12 15:15:44 -0500

    add suport classes

--- bin/Sprites.py
+++ bin/Sprites.py
@@ -14,7 +14,7 @@ class Spritesheet:
 
 class Player(pg.sprite.Sprite):
 
-    def __init__(self, stage):
+    def __init__(self, stage, type = 0):
         pg.sprite.Sprite.__init__(self)
         self.stage = stage
         self.walking = False
@@ -23,6 +23,7 @@ class Player(pg.sprite.Sprite):
         self.last_update = 0
         self.load_images()
         self.image = self.standing_frames_r[0]
+        self.mask = pg.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.center = (25, 300)
         self.pos = vec(48.5, 300) #POSITION IN ALL STAGE 
@@ -32,6 +33,7 @@ class Player(pg.sprite.Sprite):
         self.acc = vec(0, 0)
         self.des = 0
         self.side = 0
+        self.type = type
 
     def load_images(self):
         self.standing_frames_r = [self.stage.spritesheet.get_image(20, 10, 48 , 56),self.stage.spritesheet.get_image(117, 10, 48 , 56),
@@ -39,6 +41,7 @@ class Player(pg.sprite.Sprite):
                                 self.stage.spritesheet.get_image(408, 10, 48 , 56),self.stage.spritesheet.get_image(505, 10, 48 , 56)]
         for frame in self.standing_frames_r:
             frame.set_colorkey(BLACK)
+            pass
         
         self.standing_frames_l = []
         for frame in self.standing_frames_r:
@@ -71,6 +74,8 @@ class Player(pg.sprite.Sprite):
             frame.set_colorkey(BLACK)
 
     def move(self, side):
+        if self.pos.y > HEIGHT or self.pos.y < 0:
+            self.stage.loseState = True
         self.animate(side)
         #DEF GRAVITY
         self.acc = vec(0,PLAYER_GRAV)
@@ -90,7 +95,6 @@ class Player(pg.sprite.Sprite):
         # NO OUT OF STAGE
         if self.pos.x >= self.stage.stageWidth - WHP:
             self.pos.x = self.stage.stageWidth - WHP
-            self.stage.loseState = True
             self.stage.game.lose_sound()
         if self.pos.x <= WHP:
             self.pos.x = WHP
@@ -153,14 +157,18 @@ class Player(pg.sprite.Sprite):
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
 
-    def jump(self):
+    def jump(self, nivel):
         self.rect.x += 1
-        hits = pg.sprite.spritecollide(self,self.stage.platforms, False)
-        hits_floor = pg.sprite.spritecollide(self,self.stage.bases, False)
-        self.rect.x -= 1
-        if hits or hits_floor:
-            self.vel.y = -10      
-
+        if(nivel != 3):
+            hits = pg.sprite.spritecollide(self,self.stage.platforms, False)
+            hits_floor = pg.sprite.spritecollide(self,self.stage.bases, False)
+            self.rect.x -= 1
+            if hits or hits_floor:
+                self.vel.y = -10
+        else:
+            self.rect.x -= 1
+            self.vel.y = -8
+        
     def __getAcceleration(self):
         switcher = {
             1 : PLAYER_ACC_1,
@@ -170,9 +178,13 @@ class Player(pg.sprite.Sprite):
         return switcher.get(self.stage.game.difficulty)
 
     def shoot(self):
-        if self.side != 0:
-            bullet = Bullet(self.rect.x + 48, self.rect.center[1], self.side)
-            self.stage.bullets.add(bullet)
+        if self.type == 0:
+            if self.side != 0:
+                bullet = Bullet(self.rect.x + 48, self.rect.center[1], self.side)
+                self.stage.bullets.add(bullet)
+        else:
+            weapon = Weapon(self.rect.center[0],self.rect.center[1],self.type)
+            self.stage.bullets.add(weapon)
 
 class Platform(pg.sprite.Sprite):
 
@@ -225,24 +237,48 @@ class Food(pg.sprite.Sprite):
 
     def update(self , dis):
         self.rect.x -= dis
-    
+
+class Flag(pg.sprite.Sprite):
+
+    def __init__(self , x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load("../assets/one/win_flag.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+
+    def update(self , dis):
+        self.rect.x -= dis
+
 class Enemy(pg.sprite.Sprite):
     
-    def __init__(self,stage , x, y):
+    def __init__(self,stage , x, y,type):
         pg.sprite.Sprite.__init__(self)
         self.stage = stage
         self.current_frame = 0
         self.last_update = 0
-        self.load_images()
+        self.last_shoot = 0
+        self.last_move = 0
+        self.move = 1
+        self.load_images(type)
         self.image = self.standing_frames[0]
         self.rect = self.image.get_rect()
         self.rect.x = x 
         self.rect.y = y
     
-    def load_images(self):
-        standing_frames_temp = [self.stage.spritesheet_enemy.get_image(0, 0, 70 , 65),self.stage.spritesheet_enemy.get_image(70, 0, 70 , 65),
-                                self.stage.spritesheet_enemy.get_image(140, 0, 70 , 65),self.stage.spritesheet_enemy.get_image(210, 0, 70 , 65),
-                                self.stage.spritesheet_enemy.get_image(280, 0, 70 , 65),self.stage.spritesheet_enemy.get_image(350, 0, 70 , 65)]
+    def load_images(self,type):
+        if type == 1:
+            standing_frames_temp = [self.stage.spritesheet_enemy.get_image(0, 0, 70 , 65),self.stage.spritesheet_enemy.get_image(70, 0, 70 , 65),
+                                    self.stage.spritesheet_enemy.get_image(140, 0, 70 , 65),self.stage.spritesheet_enemy.get_image(210, 0, 70 , 65),
+                                    self.stage.spritesheet_enemy.get_image(280, 0, 70 , 65),self.stage.spritesheet_enemy.get_image(350, 0, 70 , 65)]
+        if type == 2:
+            standing_frames_temp = [self.stage.spritesheet_enemy.get_image(0, 0, 58 , 67),self.stage.spritesheet_enemy.get_image(58, 0, 58 , 67),
+                                    self.stage.spritesheet_enemy.get_image(116, 0, 58 , 67),self.stage.spritesheet_enemy.get_image(174, 0, 58 , 67),
+                                    self.stage.spritesheet_enemy.get_image(232, 0, 58 , 67),
+                                    self.stage.spritesheet_enemy.get_image(0, 67, 58 , 67),self.stage.spritesheet_enemy.get_image(58, 67, 58 , 67),
+                                    self.stage.spritesheet_enemy.get_image(116, 67, 58 , 67),self.stage.spritesheet_enemy.get_image(174, 67, 58 , 67),
+                                    self.stage.spritesheet_enemy.get_image(232, 67, 58 , 67)]
+            
         self.standing_frames = []
         for frame in standing_frames_temp:
             self.standing_frames.append(pg.transform.flip(frame,True, False))
@@ -251,14 +287,25 @@ class Enemy(pg.sprite.Sprite):
             frame.set_colorkey(BLACK)
 
     def animate(self):
+        now_m = pg.time.get_ticks()
+        if now_m - self.last_move > 1000:
+            self.move = self.move*-1
+            self.last_move = now_m
+        if self.rect.x > 0 and self.rect.x < WIDTH:
+                if not self.stage.pauseState and  not self.stage.loseState and not self.stage.winState:
+                    self.rect.x -= self.move*3
         now = pg.time.get_ticks()
         if now - self.last_update > 200:
             self.last_update = now
             self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
-            # bottom = self.rect.bottom
             self.image = self.standing_frames[self.current_frame]
-            # self.rect = self.image.get_rect()
-            # self.rect.bottom = bottom
+
+        now_b = pg.time.get_ticks()
+        if now_b - self.last_shoot > 3000:
+            self.last_shoot = now_b
+            if self.rect.x > 0 and self.rect.x < WIDTH:
+                bullet = BulletEnemy(self.rect.x, self.rect.center[1])
+                self.stage.bullets_enemy.add(bullet)
     
     def update(self , dis):
         self.rect.x -= dis
@@ -284,3 +331,75 @@ class Bullet(pg.sprite.Sprite):
         self.rect.x += (self.speed)*self.side
         if self.rect.x > WIDTH:
             self.kill()
+
+class BulletEnemy(pg.sprite.Sprite):
+    def __init__(self, x,y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load("../assets/one/bullet_enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.speed = 10
+    
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.x < 0:
+            self.kill()
+
+class Weapon(pg.sprite.Sprite):
+    def __init__(self, x,y,type):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load("../assets/three/p_{}.png".format(type))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.speed = 10
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+class Monster(pg.sprite.Sprite):
+    def __init__(self,stage , x, y,type,side):
+        pg.sprite.Sprite.__init__(self)
+        self.stage = stage
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images(type)
+        self.image = self.fly_frames_r[0]
+        self.mask = pg.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+        self.side = side
+    
+    def load_images(self,type):
+        self.fly_frames_r = [self.stage.spritesheet_enemy.get_image(0, 0, 98 , 65),self.stage.spritesheet_enemy.get_image(98, 0, 98 , 65),
+                                self.stage.spritesheet_enemy.get_image(196, 0, 98 , 65),self.stage.spritesheet_enemy.get_image(294, 0, 98 , 65),
+                                self.stage.spritesheet_enemy.get_image(0, 65, 98 , 65)]
+        for frame in self.fly_frames_r:
+            frame.set_colorkey(BLACK)
+            self.mask = pg.mask.from_surface(frame)
+
+        self.fly_frames_l = []
+        for frame in self.fly_frames_r:
+            self.fly_frames_l.append(pg.transform.flip(frame,True, False))
+        
+        for frame in self.fly_frames_l:
+            frame.set_colorkey(BLACK)
+            self.mask = pg.mask.from_surface(frame)
+
+    def animate(self,side):
+        now = pg.time.get_ticks()
+        if now - self.last_update > 100:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.fly_frames_r)
+            if side == 1:
+                self.image = self.fly_frames_r[self.current_frame]
+            else:
+                self.image = self.fly_frames_l[self.current_frame]
+    
+    def update(self):
+        if self.rect.x > self.stage.stageWidth or self.rect.x < -150:
+            self.kill()
+        self.animate(self.side)
+        self.rect.x += self.side * 3
